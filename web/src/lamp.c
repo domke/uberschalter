29fc6613ad929574ea7e0863e09a15b54e3297c2
c3ma <c3ma@calculator>
2014-02-20 19:50:24 +0100

    Communication with the microcontroller works

--- web/src/lamp.c
+++ web/src/lamp.c
@@ -3,7 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 #define min(a, b) ((a) < (b)) ? ((a)) : ((b))
+
+#define SER2NET_HOST	"10.23.43.31"
 
 void extractArguments(int* lamp, int* status)
 {
@@ -22,6 +27,30 @@ void extractArguments(int* lamp, int* status)
   }
 }
 
+void changeLampState(int lamp, int status)
+{
+   int sockfd,n;
+   struct sockaddr_in servaddr,cliaddr;
+   char sendline[1000];
+   char recvline[1000];
+
+   sockfd=socket(AF_INET,SOCK_STREAM,0);
+
+   bzero(&servaddr,sizeof(servaddr));
+   servaddr.sin_family = AF_INET;
+   servaddr.sin_addr.s_addr=inet_addr(SER2NET_HOST);
+   servaddr.sin_port=htons(2001);
+
+   connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+   sendto(sockfd, "ollpera\n", strlen("ollpera\n"), 0, 
+	(struct sockaddr *)&servaddr,sizeof(servaddr));
+   while(1) //FIXME endless LOOP
+   {
+      n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
+      recvline[n]=0;
+      fputs(recvline,stderr);
+   }
+}
  
 int main(void)
 {
@@ -29,6 +58,8 @@ int main(void)
   int status=0;
   /* Extract the given parameter */
   extractArguments(&lamp, &status);
+
+  changeLampState(lamp, status);
 
   printf( "Content-Type: text/plain\n\n" );
   printf("Hello from the CGI!\n");
