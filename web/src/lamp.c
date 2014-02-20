e8c4177df28fcd3e86f7942d60c9936df565f981
c3ma <c3ma@calculator>
2014-02-20 19:38:34 +0100

    Arguments from Web are extracted

--- web/src/lamp.c
+++ web/src/lamp.c
@@ -1,8 +1,35 @@
-#include "stdio.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define min(a, b) ((a) < (b)) ? ((a)) : ((b))
+
+void extractArguments(int* lamp, int* status)
+{
+  char buffer[512]; 
+  int inputLength = 0;
+  char charlength[5];  
+
+  inputLength = atoi( getenv("CONTENT_LENGTH") );
+  fprintf(stderr, "CONTENT_LENGTH=%d\n", inputLength);
+  inputLength = min( inputLength, sizeof(buffer)-1 ); /* Avoid buffer overflow */ 
+  fread( buffer, inputLength, 1, stdin );
+
+  fprintf(stderr, "Content: %s\n", buffer);
+}
+
  
-int main(void) {
+int main(void)
+{
+  int lamp=0;
+  int status=0;
+  /* Extract the given parameter */
+  extractArguments(&lamp, &status);
+
   printf( "Content-Type: text/plain\n\n" );
   printf("Hello from the CGI!\n");
-  fprintf(stderr, "Debug\n");
+  printf("LAMP=%d, STATUS=%d\n", lamp, status); //FIXME remove debugcode later
+
   return 0;
 }
